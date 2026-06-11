d820102f81e8c663ada9052469dd008b8c6628ab
SumaiyaRose <sumaiyarose600@gmail.com>
2026-06-11 17:00:21 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,19 @@
+class Printer {
+    
+    void printData(String text) {
+        System.out.println("String data: " + text);
+    }
 
+    
+    void printData(int number) {
+        System.out.println("Integer data: " + number);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        p.printData("Hello World");
+        p.printData(101);
+    }
+}
