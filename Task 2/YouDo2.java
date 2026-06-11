4c1478210236455f8afa809eb6eec28de303672a
SumaiyaRose <sumaiyarose600@gmail.com>
2026-06-11 16:58:09 +0600

    Add NumberChecker class to check number conditions

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,24 @@
+class NumberChecker {
+    void check (int num){
+        boolean condition =  (num % 2 == 0) && (num > 10);
 
+
+
+
+
+        System.out.println("Result: " + condition);
+    }
+}
+
+
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        NumberChecker checker = new NumberChecker();
+
+        checker.check(8);
+        
+    }
+    
+}
