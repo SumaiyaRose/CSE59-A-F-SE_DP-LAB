8b108523d26d422636e698167c40f06ab08bd780
SumaiyaRose <sumaiyarose600@gmail.com>
2026-06-23 11:44:16 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,57 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+
+class EmailSender implements MessageSender {
+
+    
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+class SMSSender implements MessageSender {
+
+    
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+class NotificationService {
+
+    private MessageSender messageSender;
+
+    
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+        MessageSender emailSender = new EmailSender();
+        NotificationService emailNotification =
+                new NotificationService(emailSender);
+
+        emailNotification.alertUser("Your order has shipped!");
+
+        
+        MessageSender smsSender = new SMSSender();
+        NotificationService smsNotification =
+                new NotificationService(smsSender);
+
+        smsNotification.alertUser("Your OTP is 123456");
+    }
+}
