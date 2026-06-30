e594d7636dddde20f0f2e0233ba7e09e39c15058
SumaiyaRose <sumaiyarose600@gmail.com>
2026-06-30 11:29:56 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,37 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender messageSender;
+
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+ 
+}
+
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        MessageSender emailSender = new EmailSender();
+        
+        NotificationService notificationService = new NotificationService(emailSender);
+
+        notificationService.alertUser("Your order has been shipped!");
+    }
+    
+}
 
